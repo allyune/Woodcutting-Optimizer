648234001b2372969c22180e82a5db6aecdbf190
pkrasnianska <polina.krasnianska@praguecollege.cz>
2023-05-12 16:56:21 +0200

    both placing approaches support rotating/non-rotating items

--- placement.rkt
+++ placement.rkt
@@ -1,9 +1,10 @@
 #lang racket
 
 (require "structs.rkt")
+(require math)
 (provide find-placement)
 
-
+;; Helper functions for placement by score
 (define (fits-original-orientation? item space)
     (and (<= (order-item-struct-width item) (space-struct-width space))
                          (<= (order-item-struct-height item) (space-struct-height space))))
@@ -30,15 +31,41 @@
                     (fits-original-orientation? item space))
             available-spaces)))
 
-(define (get-best-orientation item space)
+(define (compare-aspect-ratios item space)
     (define original-aspect-ratio-diff (abs (- (/ (order-item-struct-width item) (order-item-struct-height item))
-                                           (/ (space-struct-width space) (space-struct-height space)))))
+                                            (/ (space-struct-width space) (space-struct-height space)))))
 
     (define rotated-aspect-ratio-diff (abs (- (/ (order-item-struct-height item) (order-item-struct-width item))
-                                          (/ (space-struct-width space) (space-struct-height space)))))
+                                            (/ (space-struct-width space) (space-struct-height space)))))
     (if (< original-aspect-ratio-diff rotated-aspect-ratio-diff)
         'original
         'rotated))
+
+(define (get-best-orientation item space)
+    (cond
+        [(and (fits-original-orientation? item space)
+              (fits-rotated? item space))
+            (compare-aspect-ratios item space)]
+        [(fits-original-orientation? item space) 'original]
+        [(fits-rotated? item space) 'rotated]))
+
+
+;; Helper functions for placing by dispance
+(define (sort-spaces-proximity spaces)
+
+  (define (distance-squared space)
+    (let ([x (space-struct-x space)]
+          [y (space-struct-y space)])
+      (+ (* x x) (* y y))))
+  
+  (define (compare-distances space1 space2)
+    (let ([dist1 (sqrt (distance-squared space1))]
+          [dist2 (sqrt (distance-squared space2))])
+      (cond [(< dist1 dist2) -1]
+            [(> dist1 dist2) 1]
+            [else 0])))
+
+   (sort spaces compare-distances))
 
 (define (get-best-placement-by-score item sheet)
     (define best-score 0)
@@ -60,30 +87,49 @@
                     (when (> score best-score)
                         (set! best-score score)
                         (set! best-space space))))
+
     (cond
-        [(and (fits-original-orientation? item best-space)
-              (fits-rotated? item best-space))
-            (println "item fits in both orientations")
-            (println (format "best orientation is ~a" (get-best-orientation item best-space)))
-            (case (get-best-orientation item best-space)
-                ['original (println "returning placement for original orientation")(list item sheet best-space)]
-                ['rotated (list (order-item-struct 
-                                        item-height 
-                                        item-width 
-                                        (order-item-struct-material-id item)
-                                        (order-item-struct-rotate item)) 
-                                        sheet best-space)])]
-        [(fits-original-orientation? item best-space) (list item sheet best-space)]
-        [(fits-rotated? item best-space) (list (order-item-struct 
-                                                    item-height 
-                                                    item-width 
-                                                    (order-item-struct-material-id item)
-                                                    (order-item-struct-rotate item)) 
-                                                sheet best-space)]))
+            [(not (order-item-struct-rotate item))
+               (define-values (space-x space-y space-width space-height) 
+                        (values (space-struct-x best-space) (space-struct-y best-space)
+                                (space-struct-width best-space) (space-struct-height best-space)))
+               (list item sheet best-space)]
+            [else
+                (case 
+                    (get-best-orientation item best-space)
+                    ['original (list item sheet best-space)]
+                    ['rotated (list (order-item-struct 
+                                            item-height 
+                                            item-width 
+                                            (order-item-struct-material-id item)
+                                            (order-item-struct-rotate item)) 
+                                            sheet best-space)])]))
 
 ;; TODO: implement function for best placement by proximity proximity 
 (define (get-best-placement-by-proximity item sheet)
-    (void))
+    (define-values (item-width item-height material-id rotate) 
+                    (values (order-item-struct-width item) (order-item-struct-height item)
+                                (order-item-struct-material-id item) (order-item-struct-rotate item)))
+        (define valid-spaces (get-valid-spaces item sheet))
+        (define sorted-spaces (sort-spaces-proximity valid-spaces))
+        (define best-space (first sorted-spaces))
+        (cond
+            [(not (order-item-struct-rotate item))
+               (define-values (space-x space-y space-width space-height) 
+                        (values (space-struct-x best-space) (space-struct-y best-space)
+                                (space-struct-width best-space) (space-struct-height best-space)))
+               (list item sheet best-space)]
+            [else
+                (case 
+                    (get-best-orientation item best-space)
+                    ['original (list item sheet best-space)]
+                    ['rotated (list (order-item-struct 
+                                            item-height 
+                                            item-width 
+                                            (order-item-struct-material-id item)
+                                            (order-item-struct-rotate item)) 
+                                            sheet best-space)])]))
+
 
 (define (find-placement order-item sheets)
     (println (format "Getting placement for width=~a height=~a" (order-item-struct-width order-item) (order-item-struct-height order-item)))

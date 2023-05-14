a4d1636eba918e06badb2db9cde1611448506a92
pkrasnianska <polina.krasnianska@praguecollege.cz>
2023-05-14 16:51:12 +0200

    correct dropdown panel color management

---
+++ gui.rkt
@@ -0,0 +1,119 @@
+#lang racket/gui
+
+(require csv-reading
+         racket/snip	
+         "main.rkt")
+;maybe store it as a string? Depends on future usage.
+(define input-file #f)
+(define data #f)
+
+;; Styles
+(define background-background-no-file (make-object color% 234 182 118))
+(define background-background-with-file (make-object color% 36 155 98))
+(define text-color "white")
+
+;;Functions and events
+(define (browse-func)
+  (define file (get-file "Select CSV file" frame #f #f "csv" null '(("csv" "*.csv"))))
+  (when file
+    (set! input-file file)
+    (set! data (rest (csv->list (open-input-file input-file))))   
+    (send dropdown-panel refresh)))
+
+(define (start-func)
+    (with-handlers 
+        ([exn?
+            (lambda (e) (println "wrong data format")        
+            (define error (new dialog%	 
+                    [label "Input file error"]
+                    [parent frame]
+                    [width 150]
+                    [height 100]
+                    [style '(close-button)]))
+            (new message% [parent error] [label "Wrong data format, check the input file and try again"])
+            (new button% [parent error] [label "OK"] [callback (lambda (button event) (send error show #f))])
+            (send error show #t)
+            (set! input-file #f)
+            (send dropdown-panel set-canvas-background background-background-no-file)
+            (send frame refresh)
+            )])
+        (define cutting-result (process-file data))
+        (send dropdown-panel set-canvas-background background-background-with-file)
+        (send dropdown-panel refresh)
+        (define output-path (put-file "Save file to..." #f #f (make-csv-file-name) "csv"))
+        (when output-path
+            (call-with-output-file output-path
+                (lambda (output-port)
+                    (write-string (cutting-patterns->output cutting-result) output-port))))
+        (define num-sheets (hash-ref cutting-result 'number-of-sheets))
+        (define waste-percentage (hash-ref cutting-result 'waste-percentage))
+        (define output-csv (cutting-patterns->output cutting-result))
+        (send material-waste set-label (format "Waste percentage: ~a" waste-percentage))
+        (send total-sheets set-label (format "Total number of cutting sheets: ~a" num-sheets))))
+
+
+(define frame (new frame% [label "Welcome to STAKO cutter"] [width 500] [height 600]))
+
+(define my-canvas%
+  (class canvas%
+    (define/override (on-paint)
+      (let ([dc (send this get-dc)])
+        (define background-color (if input-file background-background-with-file background-background-no-file))
+        (send this set-canvas-background background-color)
+        (println (send background-color red))
+        (define-values (canvas-width canvas-height) (send this get-size))
+        (send dc set-font (make-font #:size 14 #:family 'swiss #:weight 'light))
+        (send dc set-text-foreground text-color)
+        (define text (if input-file 
+                        (match (regexp-match #rx".*/(.*)$" (path->string input-file)) [(list _ match) match]) 
+                        "Click here to select a file or drop the file to this area"))
+        (define-values (text-width text-height a b) (send dc get-text-extent text))
+        (define text-x (quotient (- canvas-width text-width) 2))
+        (define text-y (quotient (- canvas-height text-height) 2))
+        (send dc draw-text text text-x text-y)))
+
+    (define/override (on-drop-file file)
+      (set! input-file file)
+      (set! data (rest (csv->list (open-input-file input-file))))
+    ;   (send this set-canvas-background background-background-with-file)
+      (start-func)
+      (super on-drop-file file))
+
+     (define/override (on-event event)
+      (define event-type (send event get-event-type))
+      (when (eq? event-type 'left-down)
+        (define file (get-file "Select CSV file" frame #f #f "csv" null '(("csv" "*.csv"))))
+        (when file
+            (let ([dc (send this get-dc)])
+                ; (send this set-canvas-background background-background-with-file)
+                (set! input-file file)
+                (set! data (rest (csv->list (open-input-file input-file)))) 
+                (start-func))))
+      (super on-event event))
+    (super-new
+     [min-height 100]
+     [stretchable-width #t]
+     [stretchable-height #f])))
+
+; (define explorer-icon (read-bitmap "explorer.png"))
+(define dropdown-panel (new my-canvas% [parent frame] [style '(border)]
+                                       [vert-margin 10] [horiz-margin 30]
+                                       [enabled #t]
+                                       ))
+
+(define results-box (new group-box-panel%	 
+   	 	[label "Results"]	 
+   	 	[parent frame]	 
+   	 	[vert-margin 10]	 
+   	 	[horiz-margin 10]	 
+   	 	[border 2]	 
+   	 	[alignment '(left top)]	 
+   	 	[min-height 150]	 
+   	 	[stretchable-width #t]	 
+   	 	[stretchable-height #f]))
+
+(define material-waste (new message% [parent results-box] [label "Material waste: "] [stretchable-width #t]))
+(define total-sheets (new message% [parent results-box] [label "Total number of cutting sheets: "] [stretchable-width #t]))
+
+(send dropdown-panel accept-drop-files #t)
+(send frame show #t)
